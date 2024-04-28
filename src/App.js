42f929716fcc089df4c97b90fd4f6518977c9335
naksh08 <nakshatradeep21@gmail.com>
2024-04-29 01:24:04 +0530

    learnt createContext()

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import React, { useState } from "react";
 import Counter from "./Counter";
 
+export const themeContext = React.createContext();
 function App() {
-  return <Counter initialCount={69}/>;
+  const [theme, setTheme] = useState({
+    backgroundColor: "white",
+    color: "black",
+  });
+
+  return (
+    <themeContext.Provider
+      value={{ backgroundColor: theme.backgroundColor, color: theme.color }}
+    >
+      <Counter initialCount={69} />
+      <button
+        onClick={() => {
+          setTheme((prevTheme) => ({
+            backgroundColor:
+              prevTheme.backgroundColor === "white" ? "black" : "white",
+            color: prevTheme.color === "black" ? "white" : "black",
+          }));
+        }}
+      >
+        Switch Theme
+      </button>
+    </themeContext.Provider>
+  );
 }
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
+import { themeContext } from "./App";
+import { useContext } from "react";
 
 const Counter = ({initialCount}) => {
     console.log(initialCount);
     const [count, setCount] = useState(initialCount);
+
+    const style = useContext(themeContext);
+    console.log(style);
   return (
     <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}>
+      style={style}>
       <h1>Counter</h1>
-      <div>
+      <div >
         <button onClick={() => setCount(prevCount => prevCount-1) }>➖</button>
         <span> {count} </span>
         <button onClick={() => setCount(prevCount => prevCount+1)}>➕</button>
